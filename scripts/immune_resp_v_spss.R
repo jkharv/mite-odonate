39b715890b80ace10d79a6c09ca4fe6efd5caf60
Jake Harvey <jakekharvey@gmail.com>
2020-11-16 15:23:01 -0500

    This was all just garbage in the first place

--- scripts/immune_resp_v_spss.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-
-maggieRaw <- read.xlsx("Documents/Lessard Lab/Mites-Odonates/Datasets/Maggie's Data.xlsx", sheet = 2, na.strings = "N/A")
-immuneData <- subset(maggieRaw, Immune.Response > 0)
-immuneData <-immuneData[c(8:12,47)]
-
-# Spss average immune responses
-sppImm <- aggregate(immuneData[6], by = list(immuneData$G_species), FUN = mean)
-ggplot(sppImm)
