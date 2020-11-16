182ad2bf2ff193959aa5960580d99d23db58c47a
Jake Harvey <jakekharvey@gmail.com>
2020-11-16 15:18:42 -0500

    This is now functional. Assigns taxonomy, filters out non-mites and writes to a file

--- scripts/assign_taxonomy.r
+++ scripts/assign_taxonomy.r
@@ -1,11 +1,32 @@
 # Author: Jacob Harvey jakekharvey@gmail.com
 
-library("devtools")
-devtools::install_github("benjjneb/dada2", ref="v1.16", force = TRUE)
+if(!require(dada2)){
+  library("devtools")
+  devtools::install_github("benjjneb/dada2", ref="v1.16")
+}
+
 library(dada2)
+library(tidyverse)
 
 seqs <- read.csv("datasets_primary/sequencing/seqtab.nochim_MITE5.csv")
+seqs <- rename(seqs, sequence = ASV.sequence)
 mite_ref <- "datasets_derived/sequencing/reference_db.fasta"
 
-seqs_tax <- assignTaxonomy(seqs$ASV.sequence, mite_ref)
+seqs_tax <- assignTaxonomy(seqs$sequence, mite_ref, tryRC = TRUE)
+seqs_tax <- as.data.frame(seqs_tax)
+seqs_tax <- rownames_to_column(seqs_tax)
+seqs_tax <- rename(seqs_tax, sequence = rowname, phylum = Kingdom, class = Phylum, 
+                   order = Class, family = Order, genus = Family, species = Genus)
+
+s <- left_join(seqs, seqs_tax, by = "sequence")
+# only interested in mite sequences.
+mites <- filter(s, order == "Trombidiformes")
+
+outfile <- "datasets_derived/sequencing/annotated_mite_sequences.csv"
+write_csv(mites, outfile)
+
+
+
+
+
 
