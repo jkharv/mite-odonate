87e69468c44a5eb3d0054f18d0183254ef236a4b
Jake Harvey <jakekharvey@gmail.com>
2020-12-09 09:59:47 -0500

    WIP, want to move all the mess for dealing with dependencies into here

--- scripts/install_dependencies.r
+++ scripts/install_dependencies.r
@@ -1,2 +1,12 @@
-library("devtools")
-devtools::install_github("benjjneb/dada2", ref="v1.16")
+library(remotes)
+
+remotes::install_github("benjjneb/dada2", ref="v1.16")
+install.packages("BiocManager")
+install.packages("picante")
+BiocManager::install("DECIPHER")
+
+BiocManager::install("phyloseq")
+remotes::install_github("mikemc/speedyseq")
+
+
+
