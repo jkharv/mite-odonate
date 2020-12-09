c4520e2ba142cc0ad2781980c465d15e3b991eab
Jake Harvey <jakekharvey@gmail.com>
2020-12-09 11:53:16 -0500

    will clean up more, but working

--- scripts/clean_sequences.r
+++ scripts/clean_sequences.r
@@ -1,28 +1,28 @@
-# Author: Tonia de Bellis
+# Author: Tonia de Bellis / JULY 6 2020
+#
+# Cleans the raw sequences and produces and ASV and an OTU table.
+#
+library(tidyverse)
+library(reshape2)
+library(dada2)
 
-rm(list=ls()) 
+library(phyloseq)
+library(picante)
 
-# JULY 6 2020
-
-
-getwd() # print the current working directory 
-setwd(""/home/shared/tonia.debellis/MITE)
+library(DECIPHER)
+library(Biostrings)
+library(speedyseq)
+ncores = 8
 
 # Reference
 # https://benjjneb.github.io/dada2/tutorial.html
 # https://alexiscarter.github.io/metab/Dada_script.html#assignation-taxonomique-taxonomy-assignment
 # https://bioconductor.org/help/course-materials/2017/BioC2017/Day1/Workshops/Microbiome/MicrobiomeWorkflowII.html#construct_phylogenetic_tree
 
- if (!requireNamespace("BiocManager", quietly = TRUE))
- install.packages("BiocManager")
- BiocManager::install("dada2")
-
-library(dada2); packageVersion("dada2")
-
 #load("dada2_ARG_AMF_clean_MAY25.20.RData")
  
 # set file paths
-path <- "/home/shared/tonia.debellis/MITE/M06271_0035_Tonia"   # set to the directory containing the fastq files 
+path <- "datasets_primary/sequencing/raw_fastq"   # set to the directory containing the fastq files 
 
 list.files(path) # You should see the names of the fastq files.
 #"sample1-miteCOI_S343_L001_R1_001.fastq.gz" 
@@ -33,52 +33,55 @@ fnFs <- sort(list.files(path, pattern="L001_R2_001.fastq.gz", full.names = TRUE)
 fnRs <- sort(list.files(path, pattern="L001_R1_001.fastq.gz", full.names = TRUE))
 head (fnFs)
 
-
 basename(fnFs)
 # "sample1-miteCOI_S343_L001_R2_001.fastq.gz"
 #need to become "sample1-miteCOI"
 
 # Extract sample names, assuming filenames have format: SAMPLENAME_XXX.fastq
-sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 1)
 # use 1 here because want name only in part 1
 # sample1-miteCOI_(part 1) S343_(pt 2) L001_(pt 3) R2(pt 4) _001.fastq.gz (part 5)
-sample.names # now have "sample1-miteCOI"
+sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 1) %>%
+  str_subset("(20[:digit:]{2}-[:digit:]{1,2})|(Negative[:graph:]*)|(PCR[:graph:]*)") %>%
+  str_extract("(20[:digit:]{2}-[:digit:]{1,2}|(Negative-[:digit:]{1})|(PCR-neg))") %>%
+  str_replace_all("-", "_")
+sample.names # Correct format for other scripts to recognize
 
-# Plot quality profile of forward reads
-plotQualityProfile(fnFs[1:9]) # nice
-#nice to at least ~ 250 QS30
-# Plot quality profile of reverse reads
-plotQualityProfile(fnRs[1:9]) # very nice ~280
-
+if(interactive()){
+  # Plot quality profile of forward reads
+  plotQualityProfile(fnFs[1:9]) # nice
+  #nice to at least ~ 250 QS30
+  # Plot quality profile of reverse reads
+  plotQualityProfile(fnRs[1:9]) # very nice ~280
+}
 
 # must set filtered file folder path
-filt_path.MITE5 <- file.path(path, "dada2-filtered_MITE5") # Place filtered files in filtered/subdirectory
-filtFs.MITE5 <- file.path(filt_path.MITE5, paste0(sample.names, "_F_filt.fastq"))
-filtRs.MITE5 <- file.path(filt_path.MITE5, paste0(sample.names, "_R_filt.fastq"))
+filt_path.MITES <- file.path(path, "dada2-filtered_MITES") # Place filtered files in filtered/subdirectory
+filtFs.MITES <- file.path(filt_path.MITES, paste0(sample.names, "_F_filt.fastq"))
+filtRs.MITES <- file.path(filt_path.MITES, paste0(sample.names, "_R_filt.fastq"))
 
-length(filtFs.MITE5)#5
-length(filtRs.MITE5)#5
+length(filtFs.MITES)#94
+length(filtRs.MITES)#94
 
 # need to trim around 20 from start of both reads (gets rid of primer)
 # F = 26   / R = 26
 
-out.MITE5 <- filterAndTrim(fnFs, filtFs.MITE5, fnRs, filtRs.MITE5, 
+out.MITES <- filterAndTrim(fnFs, filtFs.MITES, fnRs, filtRs.MITES, 
                              trimLeft = c(26,26), truncLen=c(250,280),
                              maxN=0, maxEE=c(2,2), truncQ=2, rm.phix=TRUE,
-                             compress=TRUE, multithread=40, verbose=TRUE)  
-out.MITE5
+                             compress=TRUE, multithread= ncores, verbose=TRUE)  
+out.MITES
 
 #Filtering visualization
-filvis <- cbind((out.MITE5[,2]/out.MITE5[,1])*100) # Percentage filtered sequence / non-filtered sequence
-filvis_disc <- cbind(out.MITE5, filvis) # combines out and pourc
+filvis <- cbind((out.MITES[,2]/out.MITES[,1])*100) # Percentage filtered sequence / non-filtered sequence
+filvis_disc <- cbind(out.MITES, filvis) # combines out and pourc
 filvis_disc
 
-(mean(out.MITE5[,2])/mean(out.MITE5[,1]))*100 #93.5 Mean percentage 
+(mean(out.MITES[,2])/mean(out.MITES[,1]))*100 #94.7 Mean percentage 
 
 # Learn error rates
 # multithread argument = number of cores to use
-errF <- learnErrors(filtFs.MITE5, multithread=40)
-errR <- learnErrors(filtRs.MITE5, multithread=40)
+errF <- learnErrors(filtFs.MITES, multithread = ncores)
+errR <- learnErrors(filtRs.MITES, multithread = ncores)
 
 # sanity check - visualize error rates
 plotErrors(errF, nominalQ=TRUE)
@@ -86,16 +89,16 @@ plotErrors(errR, nominalQ=TRUE)
 
 # Dereplicate the filtered fastq files
 # Combines all identical sequencing reads into into unique sequences with a corresponding abundance. 
-derepFs <- derepFastq(filtFs.MITE5, verbose=TRUE)
-derepRs <- derepFastq(filtRs.MITE5, verbose=TRUE)
+derepFs <- derepFastq(filtFs.MITES, verbose=TRUE)
+derepRs <- derepFastq(filtRs.MITES, verbose=TRUE)
 
 # Name the derep-class objects by the sample names
 names(derepFs) <- sample.names
 names(derepRs) <- sample.names
 
 # Infer the sequence variants in each sample
-dadaFs <- dada(derepFs, err=errF,  pool="pseudo", multithread=40)
-dadaRs <- dada(derepRs, err=errR,  pool="pseudo", multithread=40)
+dadaFs <- dada(derepFs, err = errF,  pool = "pseudo", multithread = ncores)
+dadaRs <- dada(derepRs, err = errR,  pool = "pseudo", multithread = ncores)
 
 # e.g. inspect results
 dadaFs[[1]] #77 ASVs
@@ -127,7 +130,7 @@ seqtab.nochim <- removeBimeraDenovo(seqtab, method="pooled", multithread=40, ver
 dim(seqtab.nochim) # 5 177 
 sum(seqtab.nochim)/sum(seqtab) # 0.88
 
-# Lenght of the non-chimeric sequences
+# Length of the non-chimeric sequences
 hist(nchar(getSequences(seqtab.nochim)),xlab="Size", ylab="Frequency", main = "Non-chimeric ASVs length", xlim=c(240,375), ylim=c(0,300)) 
 
 #transform the ASVs occurrences in presence / absence which will allow to quantify the number of ASVs per sample.
@@ -135,13 +138,13 @@ seqtab.nochim.bin <- ifelse(seqtab.nochim>0,1,0)
 
 #following table shows how many sequences were eliminated at each step.
 getN <- function(x) sum(getUniques(x))
-track <- data.frame(Input=as.numeric(out.MITE5[,1]), # input
-                    Filtered=as.numeric(out.MITE5[,2]), # filtered
-                    "Filt//In"=as.numeric(round(((out.MITE5[,2]/out.MITE5[,1])*100),2)),# % (Filtered / Input)
+track <- data.frame(Input=as.numeric(out.MITES[,1]), # input
+                    Filtered=as.numeric(out.MITES[,2]), # filtered
+                    "Filt//In"=as.numeric(round(((out.MITES[,2]/out.MITES[,1])*100),2)),# % (Filtered / Input)
                     Merge = as.numeric(sapply(mergers, getN)), # Merged 
-                    "Mer//In"=as.numeric(round(((sapply(mergers, getN)/out.MITE5[,1])*100),2)),# % (Merged / Input)
+                    "Mer//In"=as.numeric(round(((sapply(mergers, getN)/out.MITES[,1])*100),2)),# % (Merged / Input)
                     Nonchim = as.numeric(rowSums(seqtab.nochim)),# Non-chimeric                       
-                    "Nonchim//In"=as.numeric(round(((rowSums(seqtab.nochim)/out.MITE5[,1])*100),2)),# % (Non-chimeric / Input)
+                    "Nonchim//In"=as.numeric(round(((rowSums(seqtab.nochim)/out.MITES[,1])*100),2)),# % (Non-chimeric / Input)
                     ASV = as.numeric(rowSums(seqtab.nochim.bin))) # Number of ASVs per sample 
 rownames(track) <- sample.names # Row names
 head(track) 
@@ -149,8 +152,6 @@ print(track)
 
 
 # graph it
-library(ggplot2)
-library(reshape2)
 
 gtrack<- track[,c(1,2,4,6)]
 gtrack$ID <- rownames(gtrack)
@@ -169,7 +170,7 @@ bar_track
 
 #try Steve track code again
 getN <- function(x) sum(getUniques(x))
-track_S2 <- cbind(out.MITE5, sapply(dadaFs, getN), sapply(mergers, getN), rowSums(seqtab), rowSums(seqtab.nochim))
+track_S2 <- cbind(out.MITES, sapply(dadaFs, getN), sapply(mergers, getN), rowSums(seqtab), rowSums(seqtab.nochim))
 colnames(track_S2) <- c("input", "filtered", "denoised", "merged", "tabled", "nonchim")
 rownames(track_S2) <- sample.names
 head(track_S2)
@@ -177,40 +178,21 @@ head(track_S2)
 
 # assign taxonomoy --> must blast files?
 
-save.image("dada2_Mite5_JUL6.20.RData")
+save.image("dada2_MITES_JUL6.20.RData")
 
-write.csv(seqtab.nochim, file = "seqtab.nochim_MITE5.csv")
+seqtab_asv_out <- as.data.frame(t(seqtab.nochim))
+seqtab_asv_out <- rownames_to_column(seqtab_asv_out, var = "ASV.sequence")
+  
+write.csv(seqtab_asv_out, file = "datasets_derived/sequencing/seqtab_asv.csv")
 
 
 ### Maybe try to Clump ASVs to OTUs -------------------------
-
-library(phyloseq); packageVersion("phyloseq")
-library(ggplot2); packageVersion("ggplot2")
-#install.packages("picante")
-library(picante); packageVersion("picante")
-
-
-#Cluster into OTUs
-library(tibble)
-library(dplyr)
-
-#if (!requireNamespace("BiocManager", quietly = TRUE))
-# install.packages("BiocManager")
-#BiocManager::install("DECIPHER")
-library(DECIPHER)
-library(Biostrings)
-
-#install.packages("remotes")
-library(remotes)
-# remotes::install_github("mikemc/speedyseq")
-library(speedyseq)
-
 # https://github.com/benjjneb/dada2/issues/947
 asv_sequences <- colnames(seqtab.nochim)
 sample_names <- rownames(seqtab.nochim)
 dna <- Biostrings::DNAStringSet(asv_sequences)
 
-nproc <- 40 # Increase to use multiple processors
+nproc <- ncores # Increase to use multiple processors
 aln <- DECIPHER::AlignSeqs(dna, processors = nproc)
 d <- DECIPHER::DistanceMatrix(aln, processors = nproc)
 clusters <- DECIPHER::IdClusters(
@@ -232,33 +214,35 @@ merged_seqtab <- seqtab.nochim %>%
   rowsum(clusters$cluster) %>%
   t
 
-dim(merged_seqtab) # 5 70
+dim(merged_seqtab) # 94 274
 
 
 # Optional renaming of clusters to OTU<cluster #>
 colnames(merged_seqtab) <- paste0("OTU", colnames(merged_seqtab))
 
-write.csv(merged_seqtab, file = "mergeOTU_MITE5.csv")
+seqtab_otu_out <- as.data.frame(t(seqtab.nochim))
+seqtab_otu_out <- rownames_to_column(seqtab_otu_out, var = "ASV.sequence")
+
+write.csv(merged_seqtab, file = "datasets_derived/sequencing/seqtab_otu.csv")
 
 ## Try to make a fasta file of each OTU --------------
-library(dplyr)
 #ASV is column of all the original ASV sequences and C.ASV is the new column of clustered ASVs based on the 
 # mapping to those orig. ASVs.
 
-newdf<-as.data.frame(cbind.data.frame(rownames(clusters),clusters$cluster,clusters$sequence))
-colnames(newdf)<-c("ASV","C.ASV","sequence") 
+# newdf<-as.data.frame(cbind.data.frame(rownames(clusters),clusters$cluster,clusters$sequence))
+# colnames(newdf) <- c("ASV","C.ASV","sequence")
 
-#Then arrange by C.ASV and keep only the first of duplicated ASVs 
+# #Then arrange by C.ASV and keep only the first of duplicated ASVs 
 # (there are 70 clustered ASVs  from an original 177 DADA2 ASVs):
-  
-newdf.sort<-newdf %>% arrange(C.ASV) %>% distinct(C.ASV,.keep_all = TRUE)
 
-# create a fasta file of the new OTUs for taxonomic assignment, 
-# in thier proper order (to match the OTUs in merged_seqtab) :
-  
-fas<-paste(">OTU",seq(1:nrow(newdf.sort)),"\n",newdf.sort$sequence,sep="")
+# newdf.sort <- newdf %>% arrange(C.ASV) %>% distinct(C.ASV,.keep_all = TRUE)
+
+# # create a fasta file of the new OTUs for taxonomic assignment, 
+# # in thier proper order (to match the OTUs in merged_seqtab) :
+
+# fas <- paste(">OTU",seq(1:nrow(newdf.sort)),"\n",newdf.sort$sequence,sep="")
 
 
-write.table(fas,file="MITE5_OTU.fas", row.names=FALSE, col.names=FALSE,quote = FALSE)
+# write.table(fas,file="MITES_OTU.fas", row.names=FALSE, col.names=FALSE,quote = FALSE)
 
 
