4bcd0c5c32baa8a48916021c5dc1dc90cd040eb6
Jake Harvey <jakekharvey@gmail.com>
2020-12-09 10:05:41 -0500

    Fixed mysterious problem (?) Also exludes some species that I have mites for but no phylogeny

--- scripts/make_network.r
+++ scripts/make_network.r
@@ -1,31 +1,39 @@
 # Author: Jake Harvey jakekharvey@gmail.com
-# Make a binary host-parasite network
+# Make a binary (or probabilistic) host-parasite network
 
 library(tidyverse)
 
-main <- function() {
+get_name <- function(x){
+  n <- filter(samples, sample_code == substring(x, 2))
+  if(nrow(n) == 1){
+    return(n$odonate_spp)
+  } else {
+    return(NA)
+  }
+}
+get_name <- Vectorize(get_name)
 
 samples <- read_csv("datasets_primary/mite_samples.csv")
-mites <- read_csv("datasets_derived/sequencing/annotated_mite_sequences.csv")
+mites <- read_csv("datasets_derived/sequencing/mite_sequences_annotated.csv")
 
 samples <- samples %>% 
   rename(sample_code   = "Sample Code", 
-                  odonate_spp            = "Odonate Species", 
-                  sampling_site          = "Sampling Site", 
-                  mites_no               = "Est Num. of Mites", 
-                  odonates_no            = "Num. of Odonates", 
-                  removal_date           = "Mite Removal Date", 
-                  removed_by             = "Mites Removed By", 
-                  sample_no              = "Sample No.", 
-                  extraction_date        = "DNA Extraction date", 
-                  kit_used               = "Kit Used", 
-                  preservation_treatment = "Preservation treatment", 
-                  comments               = "Comments") %>%
+         odonate_spp            = "Odonate Species", 
+         sampling_site          = "Sampling Site", 
+         mites_no               = "Est Num. of Mites", 
+         odonates_no            = "Num. of Odonates", 
+         removal_date           = "Mite Removal Date", 
+         removed_by             = "Mites Removed By", 
+         sample_no              = "Sample No.", 
+         extraction_date        = "DNA Extraction date", 
+         kit_used               = "Kit Used", 
+         preservation_treatment = "Preservation treatment", 
+         comments               = "Comments") %>%
   mutate(odonate_spp = str_replace_all(odonate_spp, " ", "_")) %>%
   mutate(sampling_site = str_replace_all(sampling_site, " ", "_"))
 
 mites <- mites %>%
-  select(contains("_")) %>%
+  select(matches("(X[[:digit:]]{4}_[[:digit:]]{1,2})")) %>% #Exclude the control samples
   t() %>%
   as.data.frame() %>%
   rownames_to_column("odonate_spp") %>%
@@ -39,17 +47,3 @@ write_csv(mites, "datasets_derived/prob_network.csv")
 
 mites <- mutate(mites, across(is.numeric, Vectorize(function(x) if(x>0){1}else{0})))
 write_csv(mites, "datasets_derived/bin_network.csv")
-
-}#main
-
-get_name <- function(x){
-  n <- filter(samples, sample_code == substring(x, 2))
-  if(nrow(n) == 1){
-    return(n$odonate_spp)
-  } else {
-    return(NA)
-  }
-}
-get_name <- Vectorize(get_name)
-
-main()
