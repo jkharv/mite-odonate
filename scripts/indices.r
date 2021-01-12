b010b2b968cf924e4503365c24d3ed3f23c518a8
Jake Harvey <jakekharvey@gmail.com>
2021-01-12 11:32:26 -0500

    Moved analysis code into it's own file.
    Seperated usage from it's implementation.

--- scripts/indices.r
+++ scripts/indices.r
@@ -1,9 +1,34 @@
 #Author: Jake Harvey (jakekharvey@gmail.com)
-#
-#
-#
 
 library(picante)
+
+source("scripts/phylo_scale.r")
+
+bin_network <- read_csv("datasets_derived/bin_network.csv")
+
+# Filtering out species that we don't have in our phylogeny.
+bin_network <- bin_network %>%
+  filter(odonate_spp != "Enallagma_annexum") %>%
+  filter(odonate_spp != "Lestes_forcipatus") %>%
+  filter(odonate_spp != "Lestes_inaequalis") %>%
+  filter(odonate_spp != "Lestes_vigilax")
+
+phylo_path <- "datasets_primary/phylogeny/FinalOdonatetree.tre"
+phylo <- read.nexus(phylo_path)
+
+#~~~~~~~~~~~~~~~~~~~Phylo scale~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Partial application to fix all the parameters but mite.
+# Also vectorize cause mutate needs this.
+partial_scale <- Vectorize(function(m){combinational_phylo_scale(bin_network, phylo, m)})
+
+mites <- tibble(mite = colnames(bin_network)[-1]) %>%
+  mutate(combo_phylo_scale = partial_scale(mite))
+
+write_csv(mites, "datasets_derived/mite_phylo_scale.csv")
+
+#~~~~~~~~~~~~~~~~~~Specialization indices~~~~~~~~~~~~~~~~~
+
 
 x<-tibble(sample = rep("A", 31), mite = bin_network$V1, odonate = bin_network$odonate_spp)
 x<sample2matrix(x)
--- scripts/phylo_scale.r
+++ scripts/phylo_scale.r
@@ -18,27 +18,6 @@ library(ape)
 library(tidyverse)
 library(mgcv)
 
-bin_network <- read_csv("datasets_derived/bin_network.csv")
-
-# Filtering out species that we don't have in our phylogeny.
-bin_network <- bin_network %>%
-  filter(odonate_spp != "Enallagma_annexum") %>%
-  filter(odonate_spp != "Lestes_forcipatus") %>%
-  filter(odonate_spp != "Lestes_inaequalis") %>%
-  filter(odonate_spp != "Lestes_vigilax")
-
-phylo_path <- "datasets_primary/phylogeny/FinalOdonatetree.tre"
-phylo <- read.nexus(phylo_path)
-
-# Partial application to fix all the parameters but mite.
-# Also vectorize cause mutate needs this.
-partial_scale <- Vectorize(function(m){combinational_phylo_scale(bin_network, phylo, m)})
-
-mites <- tibble(mite = colnames(bin_network)[-1]) %>%
-  mutate(combo_phylo_scale = partial_scale(mite))
-
-write_csv(mites, "datasets_derived/mite_phylo_scale.csv")
-
 #---------------- Combinations Method ----------------------
 
 combinational_phylo_scale <- function(network, phylo, mite){
@@ -85,21 +64,21 @@ add_to_df <- function(df, node, coef){
 #----------------- MRF GAM METHOD --------------------------
 
 # Phylo trimmed to only the species I've got in the dataset.
-trimmed_phylo <- keep.tip(phylo, bin_network$odonate_spp)
-plot(trimmed_phylo)
+#trimmed_phylo <- keep.tip(phylo, bin_network$odonate_spp)
+#plot(trimmed_phylo)
 
-pmatrix <- mrf_penalty(trimmed_phylo)
+#pmatrix <- mrf_penalty(trimmed_phylo)
 
 #get_edf <- function(){
 # interaction_presence ~ s(odonate_sp, bs= phylo_pen) )
-bin_network$odonate_spp <- factor(bin_network$odonate_spp, levels = rownames(trimmed_phylo))
-levels(bin_network$odonate_spp)
-dim(pmatrix)
-rownames(pmatrix)
-x <- gam(V38 ~ s(odonate_spp, bs = 'mrf', xt = list(penalty = pmatrix), k = 3), 
-         data = bin_network, 
-         method = "REML",
-         drop.unused.levels = FALSE,
-         family = binomial)
-summary(x)$edf
+#bin_network$odonate_spp <- factor(bin_network$odonate_spp, levels = rownames(trimmed_phylo))
+#levels(bin_network$odonate_spp)
+#dim(pmatrix)
+#rownames(pmatrix)
+#x <- gam(V38 ~ s(odonate_spp, bs = 'mrf', xt = list(penalty = pmatrix), k = 3), 
+ #        data = bin_network, 
+ #        method = "REML",
+ #        drop.unused.levels = FALSE,
+ #        family = binomial)
+#summary(x)$edf
 #}
