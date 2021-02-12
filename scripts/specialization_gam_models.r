c7a23d7fb031643a702e83c52e7dff4f3f0b09aa
Jake Harvey <jakekharvey@gmail.com>
2021-02-12 11:06:11 -0500

    GAM Models redit using mite summary data, rather than odonate summary data.
    fixes sample size issues.

--- scripts/specialization_gam_models.r
+++ scripts/specialization_gam_models.r
@@ -4,24 +4,30 @@ library(MRFtools)
 library(ape)
 library(tidyverse)
 library(mgcv)
+library(car)
 
-odonates <- read.csv("datasets_derived/odonate_summaries.csv")
+mites <- read_csv("datasets_derived/mite_summaries.csv")
 
-odonates <- odonates %>%
-  filter(mite_scale > 0)
-
-odonates$species = factor(odonates$species)
+mites$species <- factor(mites$species)
 
 phylo <- read.nexus("datasets_primary/phylogeny/FinalOdonatetree.tre")
 # Phylogeny is missing many species. I have mite data for four of the missing.
-keep <- intersect(phylo$tip.label, odonates$species)
-odonates <- filter(odonates, species %in% keep)
+keep <- intersect(phylo$tip.label, mites$species)
+mites <- filter(mites, species %in% keep)
 phylo <- keep.tip(phylo, keep)
 plot(phylo)
 
 pmatrix <- mrf_penalty(phylo)
+qqPlot(mites$mite_scale)
+hist(mites$mite_scale)
 
-x <- gam(mite_scale ~ s(species, bs = 'mrf', xt = list(penalty = pmatrix)), data = odonates,
-         method = "REML", family = gaussian)
+m <- gam(sqrt(rank(mite_scale)) ~ s(species, bs = 'mrf', xt = list(penalty = pmatrix)) + s(prevalence) + log(mass), 
+         data = mites, method = "REML", family = gaussian)
+summary(m)
 
-summary(x)
+
+x<-ggplot(aes(x = mite_scale, y = prevalence), data = mites) +
+  geom_point() + 
+  scale_y_log10() + 
+  geom_smooth()
+print(x)
