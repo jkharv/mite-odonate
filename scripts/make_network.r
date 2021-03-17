c896e35f5971cf6e3e147d1284d2261994d9181b
Jake Harvey <jakekharvey@gmail.com>
2021-03-17 16:15:11 -0400

    Conflict between two packages, changed load order to fix

--- scripts/make_network.r
+++ scripts/make_network.r
@@ -1,8 +1,9 @@
 # Author: Jake Harvey jakekharvey@gmail.com
 # Make a binary (or probabilistic) host-parasite network
 
-library(tidyverse)
 library(DESeq2)
+library(tidyverse) # Must load after DESeq2 because of conflict with DESeq2 dependancy
+library(vegan)
 
 get_name <- function(x){
   n <- filter(samples, sample_code == substring(x, 2))
@@ -33,20 +34,8 @@ samples <- read_csv("datasets_primary/mite_samples.csv")
 mites <- read_csv("datasets_derived/sequencing/mite_sequences_annotated.csv")
 
 samples <- samples %>% 
-  rename(sample_code            = "Sample Code", 
-         odonate_spp            = "Odonate Species", 
-         sampling_site          = "Sampling Site", 
-         mites_no               = "Est Num. of Mites", 
-         odonates_no            = "Num. of Odonates", 
-         removal_date           = "Mite Removal Date", 
-         removed_by             = "Mites Removed By", 
-         sample_no              = "Sample No.", 
-         extraction_date        = "DNA Extraction date", 
-         kit_used               = "Kit Used", 
-         preservation_treatment = "Preservation treatment", 
-         comments               = "Comments") %>%
-  mutate(odonate_spp = str_replace_all(odonate_spp, " ", "_")) %>%
-  mutate(sampling_site = str_replace_all(sampling_site, " ", "_"))
+  rename(c(sample_code = "Sample Code", odonate_spp = "Odonate Species")) %>%
+  mutate(odonate_spp = str_replace_all(odonate_spp, " ", "_"))
 
 controls <- mites %>%
   select(contains("neg") | "phylum" | "class" | "order" | "family" | "genus" | "species")
@@ -54,14 +43,16 @@ controls <- mites %>%
 write_csv(controls, "datasets_derived/controls.csv")
 
 mites <- mites %>%
-  select(matches("(X[[:digit:]]{4}_[[:digit:]]{1,2})")) %>% #Exclude the control samples
-  t()
+  select(matches("(X[[:digit:]]{4}_[[:digit:]]{1,2})")) #Exclude the control samples
 
 mites <- varianceStabilizingTransformation(as.matrix(mites) + 1, fitType = "local")
+#rarecurve(mites, step = 100, label = FALSE)
+#mites <- rrarefy(mites, 2500)
 
 mites <- mites %>%
+  t() %>%
   as.data.frame() %>%
-  rename_all( ~ paste0("V", .x)) %>% #DESeq2 leaves invalid col names idk.
+  #rename_all( ~ paste0("V", .x)) %>% #DESeq2 leaves invalid col names idk.
   rownames_to_column("odonate_spp") %>%
   mutate(odonate_spp = get_name(odonate_spp)) %>% # Getting the spp for each sample code
   mutate(across(is.numeric, Vectorize(function(x) if(x > 0){1}else{0}))) %>% # filter pos detec. from vst
