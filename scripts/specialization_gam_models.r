e317d1e81bbd7bd5d2b372527ecd844d3ea58f8e
Jake Harvey <jakekharvey@gmail.com>
2021-03-25 12:49:54 -0400

    minor. removed old code

--- scripts/specialization_gam_models.r
+++ scripts/specialization_gam_models.r
@@ -10,7 +10,6 @@ odonates <- read_csv("datasets_derived/odonate_summaries.csv")
 
 odonates$species <- factor(odonates$species)
 odonates <- drop_na(odonates, mite_scale_mean)
-odonates <- mutate(odonates, sr = specialist / generalist)
 
 phylo <- read.nexus("datasets_primary/phylogeny/FinalOdonatetree.tre")
 # Phylogeny is missing many species. I have mite data for four of the missing.
@@ -22,12 +21,10 @@ pmatrix <- mrf_penalty(phylo)
 
 spec_ratio <- cbind(odonates$specialist, odonates$generalist)
 
-phylo_model <- gam(spec_ratio ~ s(species, bs = 'mrf', xt = list(penalty = pmatrix), k = 10), 
-         data = odonates, method = "REML", family = binomial)
+phylo_model <- gam(spec_ratio ~ s(species, bs = 'mrf', 
+                                  xt = list(penalty = pmatrix), k = 10), 
+                   data = odonates, method = "REML", family = binomial)
 summary(phylo_model)
-
-r_model <- gam(phylo_model$residuals ~ log(mass), data = odonates, method = "REML", family = "gaussian")
-summary(r_model)
 
 mass_model <- gam(spec_ratio ~ log(mass), 
          data = odonates, method = "REML", family = binomial)
