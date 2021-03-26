fa88ce40eac6a11b3ea18cbd40f3f885dfbd3528
Jake Harvey <jakekharvey@gmail.com>
2021-03-26 10:42:16 -0400

    Added the phylo specialization indices to the mite summaries

--- scripts/mite_summaries.r
+++ scripts/mite_summaries.r
@@ -11,6 +11,8 @@ odonates <- read_csv("datasets_derived/odonate_summaries.csv")
 get_scale <- Vectorize(function(mite){mites$combo_phylo_scale[which(mites$mite == mite)]})
 get_rr <- Vectorize(function(mite){mites$resource_range[which(mites$mite == mite)]})
 get_hn <- Vectorize(function(mite){mites$num_host[which(mites$mite == mite)]})
+get_faith <- Vectorize(function(mite){mites$faiths[which(mites$mite == mite)]})
+get_simp <- Vectorize(function(mite){mites$simpson[which(mites$mite == mite)]})
 
 mite_points <- network %>% 
   pivot_longer(2:last_col()) %>%
@@ -20,7 +22,10 @@ mite_points <- network %>%
   mutate(mite_scale = get_scale(mite)) %>%
   mutate(resource_range = get_rr(mite)) %>%
   mutate(num_host = get_hn(mite)) %>%
-  select(odonate_spp, mite, mite_scale, resource_range, num_host) %>%
+  mutate(faiths = get_faith(mite)) %>%
+  mutate(simpson = get_simp(mite)) %>%
+  select(odonate_spp, mite, mite_scale, resource_range, num_host,
+         faiths, simpson) %>%
   rename(species = odonate_spp)
 
 odonates <- select(odonates, species, prevalence, mass)
