bdd769f831d08dc89b58e501d7beb9a048bf395e
Jake Harvey <jakekharvey@gmail.com>
2021-04-11 12:26:03 -0400

    Eval the assumptions

--- scripts/specialization_gam_models.r
+++ scripts/specialization_gam_models.r
@@ -5,11 +5,14 @@ library(ape)
 library(tidyverse)
 library(mgcv)
 library(car)
+library(gratia)
 
 odonates <- read_csv("datasets_derived/odonate_summaries.csv")
 
+odonates <- odonates %>% 
+  drop_na(mite_scale_mean) %>%
+  filter(generalist > 0) # no div zero
 odonates$species <- factor(odonates$species)
-odonates <- drop_na(odonates, mite_scale_mean)
 
 phylo <- read.nexus("datasets_primary/phylogeny/FinalOdonatetree.tre")
 # Phylogeny is missing many species. I have mite data for four of the missing.
@@ -17,21 +20,39 @@ keep <- intersect(phylo$tip.label, odonates$species)
 odonates <- filter(odonates, species %in% keep)
 phylo <- keep.tip(phylo, keep)
 
+#-------------------------------------------------------------------------------
+# Is there phylogenetic signal in ratio of specialists on a host?
+#
+
 pmatrix <- mrf_penalty(phylo)
+spec_gen <- cbind(odonates$specialist, odonates$generalist)
 
-spec_ratio <- cbind(odonates$specialist, odonates$generalist)
-
-phylo_model <- gam(spec_ratio ~ s(species, bs = 'mrf', 
+phylo_model <- gam(spec_gen ~ s(species, bs = 'mrf', 
                                   xt = list(penalty = pmatrix), k = 10), 
                    data = odonates, method = "REML", family = binomial)
-summary(phylo_model)
+#summary(phylo_model)
 
-mass_model <- gam(spec_ratio ~ log(mass), 
+#appraise(phylo_model)
+
+#-------------------------------------------------------------------------------
+# Does mass predict the proportion of specialists on an odonate?
+#
+
+mass_model <- gam(spec_gen ~ log(mass), 
          data = odonates, method = "REML", family = binomial)
-summary(mass_model)
+#summary(mass_model)
+
+#appraise(mass_model)
+
+#-------------------------------------------------------------------------------
+# Is specialization predicted by host abundance?
+#
 
 odonates <- drop_na(odonates, abundance)
-spec_ratio <- cbind(odonates$specialist, odonates$generalist)
-abun_model <- gam(spec_ratio ~ abundance, 
+spec_gen <- cbind(odonates$specialist, odonates$generalist)
+
+abun_model <- gam(spec_gen ~ abundance, 
                   data = odonates, method = "REML", family = binomial)
-summary(abun_model)
+#summary(abun_model)
+
+#appraise(abun_model)
