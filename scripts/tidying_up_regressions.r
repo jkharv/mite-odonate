60e81a7bf8bbad117e15f7c7cce719a6065c67a2
Jake Harvey <jakekharvey@gmail.com>
2021-04-11 12:25:42 -0400

    Couple things, eval assumptions too

--- scripts/tidying_up_regressions.r
+++ scripts/tidying_up_regressions.r
@@ -4,6 +4,7 @@ library(tidyverse)
 library(mgcv)
 library(MRFtools)
 library(ape)
+library(lmtest)
 
 a201920 <- read_csv("datasets_primary/2019-2020-mass.csv", na = c("n/a", "", "NA"))
 a2015 <- read_csv("datasets_primary/2015_data.csv", na = c("n/a", "", "NA"))
@@ -29,35 +30,47 @@ keep <- intersect(phylo$tip.label, masses$species)
 masses <- filter(masses, species %in% keep)
 phylo <- keep.tip(phylo, keep)
 
+#-------------------------------------------------------------------------------
+# Is mass phylogenetically conserved?
+#
+
 pmatrix <- mrf_penalty(phylo)
 
 # Mass is highly phylogenetically conserved. ~ 80% deviance explained. 
-mass_phylo_model <- gam(mass ~ s(species, bs = 'mrf', xt = 
+mass_phylo_model <- gam(log(mass) ~ s(species, bs = 'mrf', xt = 
                                  list(penalty = pmatrix), k = 65), 
                         data = masses, method = "REML", family = gaussian)
-summary(phylo_model)
+#summary(mass_phylo_model)
+#gam.check(mass_phylo_model)
+
+#-------------------------------------------------------------------------------
+# Is abundance phylogenetically conserved?
+#
+
+odonates <- read_csv("datasets_derived/odonate_summaries.csv") %>%
+  drop_na(abundance)
+odonates$species <- factor(odonates$species)
+
+keep <- intersect(phylo$tip.label, odonates$species)
+odonates <- filter(odonates, species %in% keep)
+abun_phylo <- keep.tip(phylo, keep)
+abun_pmatrix <- mrf_penalty(abun_phylo)
+
+abun_phylo_model <- gam(log(abundance) ~ s(species, bs = 'mrf', xt = 
+                                      list(penalty = abun_pmatrix)), 
+                        data = odonates, method = "REML", family = gaussian)
+#summary(abun_phylo_model)
+#appraise(abun_phylo_model)
+
+#-------------------------------------------------------------------------------
+# Is mite scale biased by number of hosts (sampling effort)?
+#
 
 mites <- read_csv("datasets_derived/mite_summaries.csv") %>%
   select(mite, faiths, simpson, num_host, mite_scale) %>%
   distinct()
 
-n_model <- gam(mite_scale ~ num_host, data = mites, method = "REML", 
+n_model <- gam(mite_scale ~ log(num_host), data = mites, method = "REML", 
                family = gaussian)
-summary(n_model)
-
-pdps_model <- gam(mite_scale ~ s(faiths), data = mites, method = "REML",
-                  family = gaussian)
-summary(pdps_model)
-
-summary(lm(mite_scale ~ simpson, data = mites))
-
-
-pd_plot <- ggplot(mites, aes(x = faiths, y = mite_scale)) +
-  geom_point() +
-  scale_x_log10()
-print(pd_plot)
-
-
-
-
-
+#summary(n_model)
+#appraise(n_model)
