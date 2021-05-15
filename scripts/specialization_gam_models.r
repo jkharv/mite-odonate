40399f72085dd09343f32e652845d727dbb0b4c6
Jake Harvey <jakekharvey@gmail.com>
2021-05-15 12:44:02 -0400

    Added sensitivity test for use of phylo v non-phylo index
    Simply re-runs the models with host range rather than mite scale

--- scripts/specialization_gam_models.r
+++ scripts/specialization_gam_models.r
@@ -31,8 +31,16 @@ phylo_model <- gam(spec_gen ~ s(species, bs = 'mrf',
                                   xt = list(penalty = pmatrix), k = 10), 
                    data = odonates, method = "REML", family = binomial)
 #summary(phylo_model)
-
 #appraise(phylo_model)
+
+# Sensitivity analysis for phylogenetic v non-phylogenetic indices.
+# Still significnant, but larger p and lower R sq.
+spec_gen_nhost <- cbind(odonates$specialist_nhost, odonates$generalist_nhost)
+phylo_model_nhost <- gam(spec_gen ~ s(species, bs = 'mrf', 
+                                xt = list(penalty = pmatrix), k = 10), 
+                   data = odonates, method = "REML", family = binomial)
+# summary(phylo_model)
+# appraise(phylo_model)
 
 #-------------------------------------------------------------------------------
 # Does mass predict the proportion of specialists on an odonate?
@@ -41,8 +49,14 @@ phylo_model <- gam(spec_gen ~ s(species, bs = 'mrf',
 mass_model <- gam(spec_gen ~ log(mass), 
          data = odonates, method = "REML", family = binomial)
 #summary(mass_model)
-
 #appraise(mass_model)
+
+# Sensitivity analysis
+# Becomes marginally significant. V low R sq.
+mass_model_nhost <- gam(spec_gen_nhost ~ log(mass), 
+                  data = odonates, method = "REML", family = binomial)
+# summary(mass_model_nhost)
+# appraise(mass_model_nhost)
 
 #-------------------------------------------------------------------------------
 # Is specialization predicted by host abundance?
@@ -53,6 +67,13 @@ spec_gen_abun <- cbind(odonates_abun$specialist, odonates_abun$generalist)
 
 abun_model <- gam(spec_gen_abun ~ log(abundance), 
                   data = odonates_abun, method = "REML", family = binomial)
-#summary(abun_model)
-
+summary(abun_model)
 #appraise(abun_model)
+
+# Sensitivity analysis
+# Still significant, higher R sq ????? (slightly tho, basically the same)
+spec_gen_abun_nhost <- cbind(odonates_abun$specialist_nhost, odonates_abun$generalist_nhost)
+abun_model_nhost <- gam(spec_gen_abun_nhost ~ log(abundance), 
+                        data = odonates_abun, method = "REML", family = binomial)
+# summary(abun_model_nhost)
+# appraise(abun_model_nhost)
