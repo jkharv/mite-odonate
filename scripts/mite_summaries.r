f542b255537bec2f6d414f2584bf4b7dc7252c15
Jake Harvey <jakekharvey@gmail.com>
2021-05-16 18:49:02 -0400

    Produce odonate summaries based on each ASV and OTU table.
    Also found a circular reference between mite summaries and odonate summaries. Removed that.

--- scripts/mite_summaries.r
+++ scripts/mite_summaries.r
@@ -3,38 +3,66 @@
 
 library(tidyverse)
 
-network <- read_csv("datasets_derived/bin_network.csv")
-mites <- read_csv("datasets_derived/mite_phylo_scale.csv")
-odonates <- read_csv("datasets_derived/odonate_summaries.csv")
+mite_summary <- function(network_path, indices_path){
+  
+  # network <- read_csv("datasets_derived/mite_sequences_annotated_bin_network.csv")
+  # mites <- read_csv("datasets_derived/mite_phylo_scale_asv.csv")
 
-# Get mite statistics at the host-species level.
-get_scale <- Vectorize(function(mite){mites$combo_phylo_scale[which(mites$mite == mite)]})
-get_rr <- Vectorize(function(mite){mites$resource_range[which(mites$mite == mite)]})
-get_hn <- Vectorize(function(mite){mites$num_host[which(mites$mite == mite)]})
-get_faith <- Vectorize(function(mite){mites$faiths[which(mites$mite == mite)]})
-get_simp <- Vectorize(function(mite){mites$simpson[which(mites$mite == mite)]})
+  network <- read_csv(network_path)
+  mites <- read_csv(indices_path)
+  
+  # Get mite statistics at the host-species level.
+  get_rr <- Vectorize(function(mite){mites$resource_range[which(mites$mite == mite)]})
+  get_hn <- Vectorize(function(mite){mites$num_host[which(mites$mite == mite)]})
+  get_faith <- Vectorize(function(mite){mites$faiths[which(mites$mite == mite)]})
+  get_simp <- Vectorize(function(mite){mites$simpson[which(mites$mite == mite)]})
+  
+  mite_interactions <- network %>% 
+    pivot_longer(2:last_col()) %>%
+    filter(value > 0) %>%
+    rename(mite = name) %>%
+    select(odonate_spp, mite) %>%
+    mutate(resource_range = get_rr(mite)) %>%
+    mutate(num_host = get_hn(mite)) %>%
+    mutate(faiths = get_faith(mite)) %>%
+    mutate(simpson = get_simp(mite)) %>%
+    select(odonate_spp, mite, resource_range, num_host,
+           faiths, simpson) %>%
+    rename(species = odonate_spp)
+  
+  mite_interactions <- mite_interactions %>%
+    # Spec/gen based on phylo scale.
+    mutate(specialist    = ifelse(simpson <= 100, 1, 0)) %>%
+    mutate(generalist    = ifelse(simpson >  100, 1, 0)) %>%
+    mutate(specialist_50 = ifelse(simpson <= 50, 1, 0)) %>%
+    mutate(generalist_50 = ifelse(simpson >  50, 1, 0)) %>%
+    mutate(specialist_25 = ifelse(simpson <= 25, 1, 0)) %>%
+    mutate(generalist_25 = ifelse(simpson >  25, 1, 0)) %>%
+    # Spec/gen based on nhost.  
+    mutate(specialist_nhost    = ifelse(num_host <= 2, 1, 0)) %>%
+    mutate(generalist_nhost    = ifelse(num_host >  2, 1, 0)) %>%
+    mutate(specialist_nhost_5  = ifelse(num_host <= 5, 1, 0)) %>%
+    mutate(generalist_nhost_5  = ifelse(num_host >  5, 1, 0)) %>%
+    mutate(specialist_nhost_10 = ifelse(num_host <= 10, 1, 0)) %>%
+    mutate(generalist_nhost_10 = ifelse(num_host >  10, 1, 0))
+  
+  if(grepl("otu90", indices_path)){
+    write_csv(mite_interactions, "datasets_derived/mite_summaries_otu90.csv")
+    return()
+  }
+  if(grepl("otu97", indices_path)){
+    write_csv(mite_interactions, "datasets_derived/mite_summaries_otu97.csv")
+    return()
+  }
+  if(grepl("asv", indices_path)){
+    write_csv(mite_interactions, "datasets_derived/mite_summaries_asv.csv")
+    return()
+  }
+}
 
-mite_points <- network %>% 
-  pivot_longer(2:last_col()) %>%
-  filter(value > 0) %>%
-  rename(mite = name) %>%
-  select(odonate_spp, mite) %>%
-  mutate(mite_scale = get_scale(mite)) %>%
-  mutate(resource_range = get_rr(mite)) %>%
-  mutate(num_host = get_hn(mite)) %>%
-  mutate(faiths = get_faith(mite)) %>%
-  mutate(simpson = get_simp(mite)) %>%
-  select(odonate_spp, mite, mite_scale, resource_range, num_host,
-         faiths, simpson) %>%
-  rename(species = odonate_spp)
-
-odonates <- select(odonates, species, prevalence, mass)
-
-mites <- inner_join(mite_points, odonates, by = "species")
-
-# mites <- mites %>%
-#   select(mite, mite_scale, resource_range, num_host) %>%
-#   group_by(mite) %>%
-#   summarize_all(mean)
-
-write_csv(mites, "datasets_derived/mite_summaries.csv")
+mite_summary("datasets_derived/mite_sequences_annotated_bin_network.csv",
+             "datasets_derived/mite_phylo_scale_asv.csv")
+mite_summary("datasets_derived/mite_sequences_otu90_annotated_bin_network.csv",
+             "datasets_derived/mite_phylo_scale_otu90.csv")
+mite_summary("datasets_derived/mite_sequences_otu97_annotated_bin_network.csv",
+             "datasets_derived/mite_phylo_scale_otu97.csv")
--- scripts/odonate_summaries.r
+++ scripts/odonate_summaries.r
@@ -8,62 +8,62 @@ library(lubridate)
 library(ape)
 library(phytools)
 
-odonates <- read_csv("datasets_derived/odonates_merged.csv")
-mites <- read_csv("datasets_derived/mite_summaries.csv")
+odonate_summary <- function(mites_path){
+  
+  odonates <- read_csv("datasets_derived/odonates_merged.csv")
+  mites <- read_csv(mites_path)
 
-# The 2015 data was collected using a standard sampling protocol.
-abundances <- odonates %>%
-  filter(year(date) == 2015) %>%
-  select(genus_species) %>%
-  group_by(genus_species) %>%
-  tally() %>%
-  rename(abundance = n) %>%
-  rename(species = genus_species)
+  # The 2015 data was collected using a standard sampling protocol.
+  abundances <- odonates %>%
+    filter(year(date) == 2015) %>%
+    select(genus_species) %>%
+    group_by(genus_species) %>%
+    tally() %>%
+    rename(abundance = n) %>%
+    rename(species = genus_species)
+  
+  masses <- odonates %>%
+    drop_na(mass) %>%
+    select(c(genus_species, mass)) %>%
+    mutate(n = 1) %>%
+    group_by(genus_species) %>%
+    summarise_all(sum) %>%
+    mutate(mass = mass/n) %>%
+    select(c(genus_species, mass)) %>%
+    rename(species = genus_species)
+  
+  mite_spec <- mites %>%
+    group_by(species) %>%
+    select(7:last_col()) %>%
+    summarise(across(.cols = everything(), sum))
+  
+  # Join these dataframes together
+  odonates <- list(abundances, masses, mite_spec) %>%
+    reduce(function(x, y) full_join(x, y, by = "species")) 
+  
+  # Add a column for suborder which will be useful later for plots.
+  phylo <- read.nexus("datasets_primary/phylogeny/FinalOdonatetree.tre")
+  zygoptera_mrca <- getMRCA(phylo, c("Lestes_congener", "Enallagma_hageni"))
+  zygoptera <- getDescendants(phylo, zygoptera_mrca)
+  zygoptera <- keep.tip(phylo, zygoptera)
+  
+  odonates <- mutate(odonates, suborder = if_else(species %in% zygoptera$tip.label, 
+                                                  "Zygoptera", "Anisoptera"))
+  
+  if(grepl("otu90", mites_path)){
+    write_csv(odonates, "datasets_derived/odonate_summaries_otu90.csv")
+    return()
+  }
+  if(grepl("otu97", mites_path)){
+    write_csv(odonates, "datasets_derived/odonate_summaries_otu97.csv")
+    return()
+  }
+  if(grepl("asv", mites_path)){
+    write_csv(odonates, "datasets_derived/odonate_summaries_asv.csv")
+    return()
+  }
+}
 
-masses <- odonates %>%
-  drop_na(mass) %>%
-  select(c(genus_species, mass)) %>%
-  mutate(n = 1) %>%
-  group_by(genus_species) %>%
-  summarise_all(sum) %>%
-  mutate(mass = mass/n) %>%
-  select(c(genus_species, mass)) %>%
-  rename(species = genus_species)
-
-mite_spec <- mites %>%
-  select(c(species, simpson, num_host)) %>%
-  # Spec/gen based on phylo scale.
-  mutate(specialist    = ifelse(simpson <= 100, 1, 0)) %>%
-  mutate(generalist    = ifelse(simpson >  100, 1, 0)) %>%
-  mutate(specialist_50 = ifelse(simpson <= 50, 1, 0)) %>%
-  mutate(generalist_50 = ifelse(simpson >  50, 1, 0)) %>%
-  mutate(specialist_25 = ifelse(simpson <= 25, 1, 0)) %>%
-  mutate(generalist_25 = ifelse(simpson >  25, 1, 0)) %>%
-  # Spec/gen based on nhost.  
-  mutate(specialist_nhost    = ifelse(num_host <= 2, 1, 0)) %>%
-  mutate(generalist_nhost    = ifelse(num_host >  2, 1, 0)) %>%
-  mutate(specialist_nhost_5  = ifelse(num_host <= 5, 1, 0)) %>%
-  mutate(generalist_nhost_5  = ifelse(num_host >  5, 1, 0)) %>%
-  mutate(specialist_nhost_10 = ifelse(num_host <= 10, 1, 0)) %>%
-  mutate(generalist_nhost_10 = ifelse(num_host >  10, 1, 0)) %>%
-  group_by(species) %>%
-  summarise_all(sum) %>%
-  select(c(species, specialist, generalist, specialist_25, generalist_25, 
-           specialist_50, generalist_50, specialist_nhost, generalist_nhost,
-           specialist_nhost_5, generalist_nhost_5, specialist_nhost_10, 
-           generalist_nhost_10))
-
-# Join these dataframes together
-odonates <- list(abundances, masses, mite_spec) %>%
-  reduce(function(x, y) full_join(x, y, by = "species")) 
-
-# Add a column for suborder which will be useful later for plots.
-phylo <- read.nexus("datasets_primary/phylogeny/FinalOdonatetree.tre")
-zygoptera_mrca <- getMRCA(phylo, c("Lestes_congener", "Enallagma_hageni"))
-zygoptera <- getDescendants(phylo, zygoptera_mrca)
-zygoptera <- keep.tip(phylo, zygoptera)
-
-odonates <- mutate(odonates, suborder = if_else(species %in% zygoptera$tip.label, 
-                                                "Zygoptera", "Anisoptera"))
-
-write_csv(odonates, "datasets_derived/odonate_summaries.csv")
+odonate_summary("datasets_derived/mite_summaries_otu90.csv")
+odonate_summary("datasets_derived/mite_summaries_otu97.csv")
+odonate_summary("datasets_derived/mite_summaries_asv.csv")
